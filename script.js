e3d4fc50c63bf288703ad5c55a8e46ae520d5c77
jaovitin498 <joaovitorscoparoschettino@gmail.com>
2026-03-10 16:12:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const valorReal = document.querySelector("input");
+
+function converter(){
+    const ValorConvertido = valorReal.value / 5.25
+
+    alert(`o valor em dolar é ${ValorConvertido}`);
+}
